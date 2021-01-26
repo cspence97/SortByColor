8076c5a7fd197715a1548541717eabe20854a49d
Connor <70128040+cspence97@users.noreply.github.com>
2021-01-26 13:45:44 -0600

    Update GetAlbums.py
    added ability to rip album covers from playlists

--- GetAlbums.py
+++ GetAlbums.py
@@ -2,8 +2,8 @@ import requests
 import spotipy
 import spotipy.oauth2 as oauth2
 
-client_id = "client_id"
-client_secret = "client_secret"
+client_id = "e17a6cc77e674b8180c6bb34f1b8ec7f"
+client_secret = "415d0616f5154302b12a8c56741650b8"
 
 auth = oauth2.SpotifyClientCredentials(
     client_id=client_id,
@@ -32,7 +32,7 @@ def get_artwork(info):
     file.close()
 
 
-def get_albums():
+def get_vinyls():
     # Open album list and iterate through
     f = open("AlbumList.txt")
     l = []
@@ -46,3 +46,44 @@ def get_albums():
         l.append(line.replace(" ", "-").replace("\n", ""))
     f.close()
     return l
+
+
+def get_albums(playlists):
+    urls = {}
+    if type(playlists) is not list:
+        print("Invalid playlist list")
+        return
+
+    for playlist in playlists:
+        # make sure we have good format
+        if type(playlist) is not str:
+            continue
+        # load playlist and tracks
+        results = sp.playlist(playlist)
+        tracks = results['tracks']['items']
+        # load all pages ignore error when playlist is too small and results['next'] doesn't exist
+        try:
+            while results['next']:
+                results = sp.next(results)
+                tracks.extend(results['tracks']['items'])
+        except KeyError:
+            pass
+
+        # Go through tracks and add image urls to dictionary
+        for track in tracks:
+            img = track['track']['album']['images'][0]['url']
+            if img not in urls:
+                urls[track['track']['album']['name']] = img
+                print(track['track']['album']['images'][0]['url'])
+
+        # Iterate through urls and download each one to images
+        for url in urls:
+            # open img url and download image
+            response = requests.get(urls[url])
+
+            # save image as file
+            file = open("images/" + url.replace(" ", "-") + ".jpg", "wb")
+            file.write(response.content)
+            file.close()
+
+
